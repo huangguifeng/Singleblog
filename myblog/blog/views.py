077f8c35bc3dea267da03732e594c4082377020b
hgf <957333489@qq.com>
2017-09-16 22:59:03 +0800

    s

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,4 +3,6 @@ from .models import *
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('ok')
+    bolg = Post.objects.all()
+    context = {"blog":bolg}
+    return render(request,'blog/index.html',context)
