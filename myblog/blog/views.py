5afdfff79bf779a5fad8352d26acfbc6425d8a6d
hgf <957333489@qq.com>
2017-09-16 22:56:07 +0800

    s

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,7 +3,4 @@ from .models import *
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    list = Post.objects.all()
-
-    context = {"list":list}
     return HttpResponse('ok')
