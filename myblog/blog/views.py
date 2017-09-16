cb5579003b1dec27a164fd63a4ccf73cbb95ff64
hgf <957333489@qq.com>
2017-09-16 22:46:49 +0800

    s

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,4 +3,5 @@ from .models import *
 # Create your views here.
 def index(request):
     list = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
-    return render(request,'blog/index.html',list)
+    context = {'list':list}
+    return render(request,'blog/index.html',context)
