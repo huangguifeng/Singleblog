0ebaad7d4189d3ec28e734ec7e43332778ec2867
hgf <957333489@qq.com>
2017-09-16 22:49:50 +0800

    s

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 def index(request):
-    list = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
-    context = {'list':list}
+    list = Post.objects.all()
+    context = {'list':list,}
     return render(request,'blog/index.html',context)
